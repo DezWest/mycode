e1710e9d8c4ac08c319bde4337d44aa69d412547
Dez West <originaldwho@gmail.com>
2020-10-28 15:01:45 +0000

    multiple choice game

---
+++ custif/myflix.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+message = 'Never Have I ever '
+
+#Guess which wacky question is correct for DezWest. If you have any fingers left at the end YOU WIN
+
+Q1 = input ("Never have I ever: A) Been to Asia. B) Been to South America. [A/B]? : ")
+
+Q2 = input ("Never have I ever: A) Flown a plane. B) Driven a car. [A/B]? : ")
+
+Q3 = input ("Never have I ever: A) Gone Skiing. B) Gone Snowboarding. [A/B]? : ")
+
+Q4 = input ("Never have I ever: A) Stopped skynet from coming online. B) Traveled to a galaxy far far away. [A/B]? : ")
+
+Q5 = input ("Never have I ever: A) Thought Lebron was better than Jordan. B) Dunked a basketball. [A/B]? : ")
+if Q1= A:
+print(message)"Keep fingers up"
+elif Q1= B:
+print(message) "Put one finger down"
+if Q2= B:
+print(message)"Keep fingers up"
+else: Q2= A:
+print(message)"Put one finger down"
+if Q3= B:
+print(message)"keep fingers up"
+else: Q3= A
+print(message)"Put one finger down"
+if Q4= A
+print(message)" Keep fingers up"
+else: Q4= B
+print(message)"Put one finger down"
+if Q5= A
+print(message)"Keep fingers up"
+else: Q5= B
+print(message)"Put one finger down"
