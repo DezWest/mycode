e45e99fe364a82b8d86d9177114bf7ac636556f3
Dez West <originaldwho@gmail.com>
2020-11-06 04:58:55 +0000

    pythonproject.py

--- pythonproject.py
+++ pythonproject.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python
 
-answer = input("Can you survive? (yes/no)")
+import sys
 
-while True:
+def exit_game ():
+    print("You Lose")
+    sys.exit ()
 
-    if answer.lower().strip == "yes":
+#player intro & welcome
+print("Welcome to Labrynth")
+ 
+print("Choose your best skill")
+skill = input("Type 'clairvoyance' or 'precognition': ")
 
-    answer = input("You wake up in an all white space scared and alone with two doors in front of you. Would you like to go left or right?")
+if skill == "clairvoyance":
+    input("Power to sense and communicate with paranormal activity")
+elif skill == "precognition":
+    input("Power to sense opposing danger")
+else:
+    exit_game ()
+#The game starts
 
-    if answer == "right": 
-        answer == input("You enter another white room where you encounter a being and dont know what it is. Would you like to attack or ask for help?")
+print("You wake up in an all white space scared and alone with two doors in front of you. Would you like to go left or right?")
+choice = input("Type 'left' or 'right' to proceed")
 
-        elif answer == "left"
-             answer == input("You walk through the door and fall to your death") break
+if choice == "left":
+    choice == input("You step through the door and fall to your death")
+    exit_game ()
 
-    if answer == "attack":
-        answer == input("The being transforms into a giant beast and tears you in half. You lose.") 
+elif choice == "right": 
+    choice == input("You safely pass through the door safely")
+else:
+    exit_game ()
 
-break
+print("You continue into the next room. You notice what seems to be a dark figure standing in front of a door. Your selected skill is activated")
 
-    elif answer == "ask for help":
-         answer == input("The being transforms and shows you a secret door behind him that says exit. You proceed to through the door and enter an all black room. You feel around the room and place your hand on what seems to be a flashlight.You turn the light on and see that you are in a long dark hallway that leads to a staircase. This staircase goes up one floor and down one floor. Would you like to go up or down?")  
+choice = input("Type 'clairvoyance' or 'precognition' to proceed")
 
-    else: print("You suck") 
+if choice == "clairvoyance":
+    choice == input("You ask the being to reveal itself. The being emerges from the shadow and asks what do you want? You respond with 'I am looking for a way home can you show me?' The being moves and under him reveals a secret path in which you go through")
+
+elif choice == "precognition":
+    choice == input("You recognize the being as a threat and try to avoid it. The being instantly takes notice and moves towards you. You run around the being in an attempt to escape. You run through the door the being was standing in front of and fall into the abyss. YOU HAVE DIED! Should have chosen the better power!")
+    sys.exit ()
+else:
+    exit_game ()
